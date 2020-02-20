767d10048c89bf1083da17748ef506ced0f017a3
pankaj yadav <pankaj.azm2@gmail.com>
2020-02-20 20:38:16 +0530

    crud operation react redux integrated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 var session = require('express-session'); 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 let allUsers = [];
 app.use(bodyParser.json());
@@ -15,46 +15,50 @@ app.use((req, res, next) => {
 })  
      
 app.route('/users').   
- get((req, res) => {  
- 	 // only for aunthentication
- 	 if(req.body.authentication){ 
+ get((req, res) => {    
+   // only for aunthentication
+   if(req.query.id){      
+          const isUserEdited = allUsers.filter(value => value.id === req.query.id)    
+          res.status(200).json({errorCode:false, data:isUserEdited});
+   }  
+   else if(req.body.authentication){  
           const isUserAutheticated = allUsers.filter(value => value.username === req.body.username && value.password === req.body.password)    
           res.status(200).json({errorCode:false, data:isUserAutheticated});
- 	 }
- 	 else{   
-     		res.status(200).json({ errorCode:false, data:allUsers });  
+   }
+   else{   
+        res.status(200).json({ errorCode:false, data:allUsers });  
         }
  }).    
  patch((req, res) => {     
 
- 	const getUpdatedData = session; 	
- 	session = getUpdatedData.map(value  => (value.id === req.body.id) ? 
- 		{
- 			...value,
- 			username:req.body.username,
- 			password:req.body.password,
- 		}   
- 		: value);
+  const getUpdatedData = session;   
+  session = getUpdatedData.map(value  => (value.id === req.body.id) ? 
+    {
+      ...value,
+      username:req.body.username,
+      password:req.body.password,
+    }   
+    : value);
     allUsers = session;
- 	res.status(200).send({errorCode:false, errorMessage:'Data Updated Successfully'});   
+  res.status(200).send({errorCode:false, errorMessage:'Data Updated Successfully'});   
  }). 
- post((req, res)=>{      
- 	if(req.query.username && req.query.age && req.query.address){ 
- 		console.log('sdfsdfdfsa'); 
- 		allUsers.push({username:req.query.username, age:req.query.age, id:Math.random(), address:req.query.address});
- 		session = allUsers;     
- 	}       
-	res.status(200).send({errorCode:false, data:session, errorMessage:'Data Inserted Successfully'});  
+ post((req, res)=>{  
+ console.log('allUsers',allUsers);     
+  if(req.query.username && req.query.age && req.query.address){ 
+    allUsers.push({username:req.query.username, age:req.query.age, id:Math.floor(Math.random() * 10), address:req.query.address});
+    session = allUsers;     
+  }        
+  res.status(200).send({errorCode:false, data:session, errorMessage:'Data Inserted Successfully'});  
  }).        
- delete((req, res) => {   
- 	const deletedData = session;
- 	session = deletedData.filter(value => value.id !== req.query.id);  
- 	allUsers = session;    
-	res.status(200).send({errorCode:false, data:session, errorMessage:'Data Deleted Successfully'});   
- });   
+ delete((req, res) => {    
+  const deletedData = allUsers.filter(value => value.id != req.query.id);     
+  session  = deletedData; 
+  allUsers = session;                     
+  res.status(200).send({errorCode:false, data:deletedData, errorMessage:'Data Deleted Successfully'});   
+ });        
   
 app.use(function(req, res){
-	res.status(404).send({url:req.originalUrl + ' not found'});  
+  res.status(404).send({url:req.originalUrl + ' not found'});  
 })
 
 app.listen(port);    
